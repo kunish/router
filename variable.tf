52cbb2eecb516d32414ed950c62190bd59a1e1de
shikun <kunish.butt@gmail.com>
2021-07-22 02:51:46 +0800

    feat: create lease and record from variables

--- variable.tf
+++ variable.tf
@@ -12,3 +12,15 @@ variable "mikrotik_password" {
   type      = string
   sensitive = true
 }
+
+variable "domain_name" {
+  type = string
+}
+
+variable "hosts" {
+  type = map(object({
+    name       = string
+    macaddress = string
+    address    = string
+  }))
+}
